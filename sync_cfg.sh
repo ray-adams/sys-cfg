b8d5bc6a04bc19b2de26dfd0d9acc8641fd5a469
ray <ray@salaciouswind.com>
2024-07-05 15:44:00 +0100

    Adding angelica to sync script

--- sync_cfg.sh
+++ sync_cfg.sh
@@ -7,7 +7,7 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 2.1.1
+# Version: 2.3.1
 
 # Obtain the path for <git_root>
 working_dir="$(git rev-parse --show-toplevel)"
@@ -22,15 +22,14 @@ sync() {
 
     ## chrony
     mkdir -p "${working_dir}/${system}/chrony/"
-    echo "${green}Syncing /etc/chrony for ${system}.${nc}"
     rsync -qru --delete \
         --exclude ".*" \
         "${system}:/etc/chrony/" \
         "${working_dir}/${system}/chrony/" || { echo "${red}Syncing /etc/chrony for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/chrony for ${system} successfully.${nc}"
 
     ## conf.d
     mkdir -p "${working_dir}/${system}/conf.d/"
-    echo "${green}Syncing conf.d configuration for ${system}.${nc}"
     rsync -qu \
         "${system}:/etc/conf.d/hostname" \
         "${system}:/etc/conf.d/keymaps" \
@@ -39,40 +38,31 @@ sync() {
 
     if [ "${system}" = "kotori" ]; then
         rsync -qu \
-        "${system}:/etc/conf.d/syncthing" \
-        "${working_dir}/${system}/conf.d/" || { echo "${red}Syncing syncthing configuration for ${system} failed.${nc}"; exit 1; }
+            "${system}:/etc/conf.d/syncthing" \
+            "${working_dir}/${system}/conf.d/" || { echo "${red}Syncing syncthing configuration for ${system} failed.${nc}"; exit 1; }
     fi
-
-    ## libvirt
-    if [ "${system}" = "kotori" ]; then
-        mkdir -p "${working_dir}/${system}/libvirt/"
-        echo "${green}Syncing libvirt configuration for ${system}.${nc}"
-        rsync -qu \
-            "${system}:/etc/libvirt/libvirtd.conf" \
-            "${working_dir}/${system}/libvirt/" || { echo "${red}Syncing libvirt configuration for ${system} failed.${nc}"; exit 1; }
-    fi
+    echo "${green}Synced conf.d configuration for ${system} successfully.${nc}"
 
     ## modules-load.d
     mkdir -p "${working_dir}/${system}/modules-load.d/"
-    echo "${green}Syncing /etc/modules-load.d for ${system}.${nc}"
     rsync -qru --delete \
         "${system}:/etc/modules-load.d/" \
         "${working_dir}/${system}/modules-load.d/" || { echo "${red}Syncing modules-load.d configuration for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/modules-load.d for ${system} successfully.${nc}"
 
     ## pam.d
     mkdir -p "${working_dir}/${system}/pam.d/"
-    echo "${green}Syncing pam.d configuration for ${system}.${nc}"
     rsync -qu \
         "${system}:/etc/pam.d/doas" \
         "${system}:/etc/pam.d/su" \
         "${system}:/etc/pam.d/system-local-login" \
         "${working_dir}/${system}/pam.d/" || { echo "${red}Syncing pam.d configuration for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced pam.d configuration for ${system} successfully.${nc}"
 
     ## Portage
 
     ### Sync subdirectories, files within them (excluding "dot" files), and do not delete certain files.
-    mkdir -p "${working_dir}/${system}/portage/savedconfig/"
-    echo "${green}Syncing /etc/portage for ${system}.${nc}"
+    mkdir -p "${working_dir}/${system}/portage/"
     rsync -qru --delete \
         --exclude ".*" \
         --exclude "make.conf" \
@@ -93,62 +83,90 @@ sync() {
         "${system}:/etc/portage/package.unmask" \
         "${system}:/etc/portage/repos.conf" \
         "${working_dir}/${system}/portage/" || { echo "${red}Syncing /etc/portage for ${system} failed.${nc}"; exit 1; }
-
-    ### linux-firmware.
-    echo "${green}Syncing linux-firmware configuration for ${system}.${nc}"
-    rsync -qu \
-        "${system}:/etc/portage/savedconfig/sys-kernel/linux-firmware" \
-        "${working_dir}/${system}/portage/savedconfig/sys-kernel/" || { echo "${red}Syncing linux-firmware configuration for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/portage for ${system} successfully.${nc}"
 
     ## stubby
     mkdir -p "${working_dir}/${system}/stubby/"
-    echo "${green}Syncing /etc/stubby for ${system}.${nc}"
     rsync -qru --delete \
         --exclude ".*" \
         "${system}:/etc/stubby/" \
         "${working_dir}/${system}/stubby/" || { echo "${red}Syncing /etc/stubby for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/stubby for ${system} successfully.${nc}"
 
     ## zsh
     mkdir -p "${working_dir}/${system}/zsh/"
-    echo "${green}Syncing /etc/zsh/zshenv for ${system}.${nc}"
     rsync -qu \
         "${system}:/etc/zsh/zshenv" \
         "${working_dir}/${system}/zsh/" || { echo "${red}Syncing /etc/zsh/zshenv for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/zsh/zshenv for ${system} successfully.${nc}"
 
     # Files
 
-    # doas.conf
-    echo "${green}Syncing /etc/doas.conf for ${system}.${nc}"
+    ## doas.conf
     rsync -qu \
         "${system}:/etc/doas.conf" \
         "${working_dir}/${system}/" || { echo "${red}Syncing /etc/doas.conf for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/doas.conf for ${system} successfully.${nc}"
 
-    # environment
-    echo "${green}Syncing /etc/environment for ${system}.${nc}"
+    ## environment
     rsync -qu \
         "${system}:/etc/environment" \
         "${working_dir}/${system}/" || { echo "${red}Syncing /etc/environment for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/environment for ${system} successfully.${nc}"
 
-    # hostname
-    echo "${green}Syncing /etc/hostname for ${system}.${nc}"
+    ## hostname
     rsync -qu \
         "${system}:/etc/hostname" \
         "${working_dir}/${system}/" || { echo "${red}Syncing /etc/hostname for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/hostname for ${system} successfully.${nc}"
 
-    # resolv.conf.head
-    echo "${green}Syncing /etc/resolv.conf.head for ${system}.${nc}"
+    ## resolv.conf.head
     rsync -qu \
         "${system}:/etc/resolv.conf.head" \
         "${working_dir}/${system}/" || { echo "${red}Syncing /etc/resolv.conf.head for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/resolv.conf.head for ${system} successfully.${nc}"
 
-    # systctl.conf
-    echo "${green}Syncing /etc/sysctl.conf for ${system}.${nc}"
+    ## systctl.conf
     rsync -qu \
         "${system}:/etc/sysctl.conf" \
         "${working_dir}/${system}/" || { echo "${red}Syncing /etc/sysctl.conf for ${system} failed.${nc}"; exit 1; }
+    echo "${green}Synced /etc/sysctl.conf for ${system} successfully.${nc}"
+
+    # System Specific
+
+    ## dhcpcd.conf
+    if [ "${system}" = "angelica" ]; then
+        rsync -qu \
+            "${system}:/etc/dhcpcd.conf" \
+            "${working_dir}/${system}/" || { echo "${red}Syncing dhcpcd.conf for ${system} failed.${nc}"; exit 1; }
+        echo "${green}Synced /etc/dhcpcd.conf successfully.${nc}"
+    fi
+
+    ## libvirt
+    if [ "${system}" = "kotori" ]; then
+        mkdir -p "${working_dir}/${system}/libvirt/"
+        rsync -qu \
+            "${system}:/etc/libvirt/libvirtd.conf" \
+            "${working_dir}/${system}/libvirt/" || { echo "${red}Syncing libvirt configuration for ${system} failed.${nc}"; exit 1; }
+        echo "${green}Synced libvirt configuration for ${system} successfully.${nc}"
+    fi
+
+    ## linux-firmware.
+    if [ "${system}" != "angelica" ]; then
+        mkdir -p "${working_dir}/${system}/portage/savedconfig/"
+        rsync -qu \
+            "${system}:/etc/portage/savedconfig/sys-kernel/linux-firmware" \
+            "${working_dir}/${system}/portage/savedconfig/sys-kernel/" || { echo "${red}Syncing linux-firmware configuration for ${system} failed.${nc}"; exit 1; }
+        echo "${green}Synced linux-firmware configuration for ${system} successfully.${nc}"
+    fi
 }
 
 case ${1} in
+    angelica)
+        system="angelica"
+        sync
+    ;;
+
     kotori)
         system="kotori"
         sync
@@ -162,6 +180,6 @@ case ${1} in
     *)
         echo "Invalid option: \"${1}\""
         echo "Correct Usuage: ${0} [SYSTEM]"
-        echo "Available systems: eleanore-compile, kotori"
+        echo "Available systems: angelica, eleanore-compile, kotori"
     ;;
 esac
